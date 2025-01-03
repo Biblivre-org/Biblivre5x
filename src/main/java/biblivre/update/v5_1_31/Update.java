2cda9a3889b976bc3253c1f5e9be15c6d515c3ea
wlucas <xmidia@gmail.com>
2025-01-03 14:18:21 -0300

    Servidores BR z3950 não estão atualizando em todos os casos, portanto, esta desabilitado.

--- src/main/java/biblivre/update/v5_1_31/Update.java
+++ src/main/java/biblivre/update/v5_1_31/Update.java
@@ -127,11 +127,12 @@ public class Update implements UpdateService {
 			z3950.add("INSERT INTO single.z3950_addresses (id, name, url, port, collection) "
 					+ "VALUES ('1','3 - BIBLIOTECA NACIONAL (BIBLIOGRÁFICO)','152.70.215.55','9998','bib');");
 			z3950.add("INSERT INTO single.z3950_addresses (id, name, url, port, collection) "
-					  + "VALUES ('2','4 - BIBLIOTECA NACIONAL (AUTORIDADE)','152.70.215.55','9998','aut');"); */
+					  + "VALUES ('2','4 - BIBLIOTECA NACIONAL (AUTORIDADE)','152.70.215.55','9998','aut');"); 
 			z3950.add("INSERT INTO single.z3950_addresses (id, name, url, port, collection) "
 					  + "VALUES ('3','1 - Universidade Federal de Santa Catarina (UFSC)','z3950.ufsc.br','210','Default');");
 			z3950.add("INSERT INTO single.z3950_addresses (id, name, url, port, collection) "
-					  + "VALUES ('4','2 - Biblioteca do Senado','biblioteca2.senado.gov.br','9991','sen01');");			
+					  + "VALUES ('4','2 - Biblioteca do Senado','biblioteca2.senado.gov.br','9991','sen01');");	
+		*/			  		
 			
 			
 	  return z3950;
